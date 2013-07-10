4aeb6c49039d7182369edc6c297a2c0bff83c8f1
Ubuntu <ubuntu@ip-172-31-42-186.us-west-2.compute.internal>
2013-07-10 20:10:57 +0000

    grader with restler

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -65,8 +66,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file>','Url path')
         .parse(process.argv);
+if(program.url){
+    htmlfile = "web.html";
+    rest.get(program.url).on('complete', function(result){
+        fs.writeFileSync(htmlfile, result);
+     });
+    var checkJson = checkHtmlFile(htmlfile, program.checks);
+}
+else{
     var checkJson = checkHtmlFile(program.file, program.checks);
+}    
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
